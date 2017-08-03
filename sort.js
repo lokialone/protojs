53703d16481b8e0e5393089ed266d77384495a2c
lokialone <648875862@qq.com>
2017-08-03 14:52:59 +0800

    插入排序

--- sort.js
+++ sort.js
@@ -2,8 +2,10 @@
 //1.实现一个类似原生的sort，可以接受函数来做sort的依据
 //2. 与原生的sort函数运行时间进行比较
 //3.todo 快排
-let arr = [1, 2, 4, 5, 7, 8, 9, 10, 0, 4];
+//4.理解冒泡函数的原理
+let arr = [1, 2, 4, 5, 7, 8, 9, 10, 0, 4, 44, 3, 55, 5, 5, 29, 40];
 
+// 冒泡
 function sort1(arr, fun) {
     if (!fun) {
         fun = function (a, b) {
@@ -24,9 +26,33 @@ function sort1(arr, fun) {
 
 
 
-console.time('time2');
+console.time('origin');
 arr.sort();
-console.timeEnd('time2');
-console.time('time1');
-sort1(arr);
-console.timeEnd('time1');
+console.timeEnd('origin');
+// console.time('time1');
+// sort1(arr);
+// console.timeEnd('time1');
+
+
+//插入排序
+function sort2(arr) {
+    let resArray = [];
+    arr.forEach((item) => {
+        if (!resArray.length) {
+            resArray.push(item);
+        } else {
+            let needIndex = 0;
+            resArray.every((item2, index) => {
+                if (item < item2) {
+                    needIndex = index;
+                    return false
+                }
+                return true;
+            });
+            resArray.splice(needIndex, 0, item);
+        }
+    });
+    return resArray;
+}
+
+console.log(sort2(arr));

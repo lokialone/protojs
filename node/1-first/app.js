b6f65d261fb38e0bc7d721062a5882dc71c43be2
lokialone <648875862@qq.com>
2018-03-21 00:29:45 +0800

    feat: http client

--- node/1-first/app.js
+++ node/1-first/app.js
@@ -24,6 +24,24 @@ var filterDir = require('./filter-dir.js');
 //     }
 // });
 
+http.get(process.argv[2], (resp) => {
+    // Do stuff with response
+    // A chunk of data has been recieved.
+    resp.setEncoding('utf8')
+    resp.on('data', (chunk) => {
+     console.log(chunk);
+     
+    });
+ 
+    // The whole response has been received. Print out the result.
+    // resp.on('end', () => {
+    //     console.log(data);
+    // });
+ 
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+});
+
 
 
 

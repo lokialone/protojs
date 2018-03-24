4bb26e62880e6ac7c5403d771cf91d188cf058ec
lokialone <648875862@qq.com>
2018-03-24 23:54:59 +0800

    feat(node): http server

--- node/1-first/app.js
+++ node/1-first/app.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var filterDir = require('./filter-dir.js');
-
+const bl = require('bl')
 // 获取文件内容的行数 类似cat file | wc -l
 // let num =  fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
 // fs.readFile(process.argv[2], 'utf8', (err, data) => {
@@ -24,23 +24,84 @@ var filterDir = require('./filter-dir.js');
 //     }
 // });
 
-http.get(process.argv[2], (resp) => {
-    // Do stuff with response
-    // A chunk of data has been recieved.
-    resp.setEncoding('utf8')
-    resp.on('data', (chunk) => {
-     console.log(chunk);
-     
+// http.get(process.argv[2], (resp) => {
+//     // Do stuff with response
+//     // A chunk of data has been recieved.
+//     // resp.setEncoding('utf8')
+//     var bl = new BufferList();
+//     resp.on('data', (chunk) => {
+//         bl.append(chunk);
+//     });
+ 
+//     // The whole response has been received. Print out the result.
+//     resp.on('end', () => {
+//         console.log(bl.length);
+//         console.log(bl.toString());
+//     });
+ 
+//     }).on("error", (err) => {
+//         console.log("Error: " + err.message);
+// });
+
+
+// http.get(process.argv[2], (res) => {
+//     res.pipe(bl((err, data) => {
+//         if (err) return console.error(err)
+//         data = data.toString()
+//         console.log(data.length);
+//         console.log(data);     
+//     }));
+// })
+var urls = []
+function collectArgvUrl() { 
+    process.argv.forEach((item, index) => {
+        if (index > 1) urls.push(item);
     });
- 
-    // The whole response has been received. Print out the result.
-    // resp.on('end', () => {
-    //     console.log(data);
-    // });
- 
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-});
+}
+collectArgvUrl();
+// const urls = ['http://127.0.0.1:8080/filter-dir.js', 'http://127.0.0.1:8080/text', 'http://127.0.0.1:8080/text2']
+
+// use recursion to do this
+// function getRes(url, index) {
+//     if (index > urls.length) return;
+//     http.get(url, (res) => {
+//         res.pipe(bl((err, data) => {
+//             if (err) return console.error(err)
+//             data = data.toString()
+//             console.log(data);
+//             index = index + 1;
+//             getRes(urls[index], index)
+//         }));
+//     })
+// }
+// getRes(urls[0], 0);
+// get response in order;
+var results = []
+var count  = []
+function printResults() {
+    for (var i = 0; i < results.length; i++) {
+        console.log(results[i])
+    } 
+}
+
+function httpGet(i) {
+    http.get(process.argv[2 + i], (res => {
+        res.pipe(bl((err, data) => {
+            if (err) return console.error(err)
+            results[i] = data.toString();
+            count++;
+            if (count === url.length) {
+                printResults();
+            }
+        }))
+    }))
+}
+
+for (var i = 0; i < urls.length; i++) {
+    httpGet(urls[i])
+  }
+
+
 
 
 

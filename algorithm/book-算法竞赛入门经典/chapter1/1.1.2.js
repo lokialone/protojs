e4db11ee7e0f925f54cd990a6386af18063e9be9
lokialone <648875862@qq.com>
2018-05-07 15:44:36 +0800

    feat: timeAssign

--- algorithm/book-算法竞赛入门经典/chapter1/1.1.2.js
+++ algorithm/book-算法竞赛入门经典/chapter1/1.1.2.js
@@ -1,7 +1,8 @@
 // p2 时间分配
 //只关注算法，不考虑如何输入数据
 require('../tool.js');
-let testData = [{n: 3, j: 3}, {n: 4, j: 4}, {n: 5, j: 5}]; 
+let testData = [{n: 3, j: 3}, {n: 4, j: 4}, {n: 5, j: 5}];
+let testData_1 = [{n: 2,j : 5}, {n: 4, j: 2}, { n: 5, j: 3}]
 // log 15
 // step1 把数组根据j的大小重大到小排序
 // 算出同步执行任务，需要多花的额外时间
@@ -33,13 +34,24 @@ function timeAssgin(data) {
     res += extraTime;
     console.log(res);
 }
-
-// timeAssgin(testData);
-
+// for my verison
+timeAssgin(testData);
+timeAssgin(testData_1);
+// 不是去算需要多花的额外时间，而是去找额外需要的最大时间
 function timeAssgin_2(data) {
     data.sort((a, b) => {
         return a.j < b.j;
     });
-    
+    let ans = 0;
+    let s = 0;
+    data.each((item, index) => {
+        s += item.n;
+        ans = Math.max(ans, s + item.j);
+    });
+    console.log(ans);  
 }
+// for my better version
+timeAssgin_2(testData);
+timeAssgin_2(testData_1);
+
 

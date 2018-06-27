ead2862a429cab7289a1b046d18e4706f143882f
loki <648875862@qq.com>
2018-06-27 13:49:28 +0800

    feat: get Authcode

--- node/email/index.js
+++ node/email/index.js
@@ -6,8 +6,11 @@ const fs = require('fs')
 const path = require('path')
 const inquirer = require('./lib/inquirer')
 const checkInfo = require('./lib/check-init-info')
-const http = require('http');
-// const files = require('./lib/files')
+const http = require('http')
+const CLI = require('clui')
+const Spinner = CLI.Spinner
+var countdown = new Spinner('发送请求中  ', ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']);
+
 const log = console.log
 
 const rl = readline.createInterface({
@@ -22,14 +25,37 @@ log(
       figlet.textSync('deploy-email', { horizontalLayout: 'full' })
     )
 )
+// 获取邮件组
+// function getEmailGroup() {
+//     rl.on('line', (line) => {
+//         emailGroup.push(line)
+//     });
+// }
 
-function getEmailGroup() {
-    rl.on('line', (line) => {
-        emailGroup.push(line)
-    });
-}
+// getEmailGroup()
 
-getEmailGroup()
-inquirer.askBasicInfo()
+// 输入项目信息
+// inquirer.askBasicInfo()
 
-checkInfo.init()
+// checkInfo.init()
+countdown.start()
+setTimeout(() => {
+    log(
+        chalk.blue.bold('验证码发送成功')
+    )
+    countdown.stop()
+}, 2000);
+
+// http.get('http://sso.dasouche-inc.net/PhoneCode.json?username=15757129510&isDingDing=true', function(res) { 
+//     if (res.statusCode == '200') {
+//         log(
+//             chalk.blue.bold('验证码发送成功')
+//         )
+//     }
+//   }).on('error', function(e) {
+//     log(
+//         chalk.red.bold(e.message)
+//     )
+//   })
+
+http.post('http://sso.dasouche-inc.net/loginAction/phoneCheck.do')
---
+++ node/email/lib/auth.js
@@ -0,0 +1,4 @@
+const http = require('http')
+http://sso.dasouche-inc.net/PhoneCode.json?username=15757129510&isDingDing=true
+
+http.request()

926cea0536de387d3de9d7fe4038866500e58931
loki <648875862@qq.com>
2018-08-22 23:57:57 +0800

    feat: init odin

---
+++ node/Odin/index.js
@@ -0,0 +1,14 @@
+import Swagger from 'swagger-client';
+
+Swagger.http()
+// http://topgear.prepub.souche.com/api-docs/
+const request = {
+    url,
+    query,
+    method,
+    body,
+    headers,
+    requestInterceptor,
+    responseInterceptor,
+    userFetch
+  }

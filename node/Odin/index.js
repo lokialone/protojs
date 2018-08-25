5ea283a9fcdd6f9692f3e71e19db6913a1f2ac1b
loki <648875862@qq.com>
2018-08-25 10:58:01 +0800

    feat: create react

--- node/Odin/index.js
+++ node/Odin/index.js
@@ -1,14 +1,57 @@
-import Swagger from 'swagger-client';
+const axios = require('axios');
 
-Swagger.http()
-// http://topgear.prepub.souche.com/api-docs/
-const request = {
-    url,
-    query,
-    method,
-    body,
-    headers,
-    requestInterceptor,
-    responseInterceptor,
-    userFetch
+const baseUrl = 'http://topgear.prepub.souche.com/api-docs/';
+
+axios.defaults.headers.get['Cookie'] = '_security_token=1JxnP_bCwFWRGbmI';
+
+/**
+ *
+ * @param {*} baseUrl
+ */
+async function getAPiDocsInfo(baseUrl) {
+  try {
+      const res = await axios.get(baseUrl)
+      // console.log(res);
+      if (res.data) return res.data.apis
+      // TODO
+      // 登录souche
+      // then reget docinfo
+
+  } catch (error) {
+      console.log(error)
   }
+}
+
+// TODO
+// 登录souche
+function login() {
+
+}
+
+function createSchema() {
+
+}
+
+async function init () {
+	const docsInfo = await getAPiDocsInfo(baseUrl);
+	// for get api-docs info concurrently
+	const apiInfo= docsInfo.map(async item => {
+		let data = await axios.get(baseUrl + item.path)
+    	return data.data
+  	});
+	// get all apis and models
+	const tmp = []
+  	for (let info of apiInfo) {
+    	tmp.push(await info)
+	}
+	console.log(tmp[1])
+}
+
+// init();
+const basicType = [
+	'string', 'boolean', 'integer','object'
+]
+
+
+
+

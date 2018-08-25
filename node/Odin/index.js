98ff282fd13a61b38f059e6a7669da708d7b1cd9
loki <648875862@qq.com>
2018-08-26 00:23:44 +0800

    feat: update

--- node/Odin/index.js
+++ node/Odin/index.js
@@ -1,8 +1,11 @@
-const axios = require('axios');
+const axios = require('axios')
+const _ = require('lodash')
+const idx = require('idx')
+const testData = require('./testData')
 
-const baseUrl = 'http://topgear.prepub.souche.com/api-docs/';
+const baseUrl = 'http://topgear.prepub.souche.com/api-docs/'
 
-axios.defaults.headers.get['Cookie'] = '_security_token=1JxnP_bCwFWRGbmI';
+axios.defaults.headers.get['Cookie'] = '_security_token=1JxnP_bCwFWRGbmI'
 
 /**
  *
@@ -28,10 +31,6 @@ function login() {
 
 }
 
-function createSchema() {
-
-}
-
 async function init () {
 	const docsInfo = await getAPiDocsInfo(baseUrl);
 	// for get api-docs info concurrently
@@ -44,13 +43,77 @@ async function init () {
   	for (let info of apiInfo) {
     	tmp.push(await info)
 	}
-	console.log(tmp[1])
 }
 
 // init();
-const basicType = [
-	'string', 'boolean', 'integer','object'
-]
+/**
+ * create api schema
+ * @param {*} data
+ * return object
+ */
+let allModel = {}
+function createSchema(data) {
+	const api = idx(data, _ => _.apis[0].path);
+	// response对象
+	const response = idx(data, _ => _.apis[0].operations[0].type)
+	allModel = data.models
+
+	if (!api || !response || !allModel) {
+		console.error('解析api model出错: ' + api + response)
+		return {}
+	}
+
+	//response data 对象 需要验证的对象
+	const reData= idx(allModel, _ => _[response].properties.data)
+	if (!reData) {
+		console.error('未返回shcema: ' + api + reData)
+	}
+	console.log('result', dealSchema(reData));
+}
+
+
+
+function dealSchema(data, schema = {}) {
+	if (_.isUndefined(data)) return schema
+
+	if (_.has(data, 'properties')) {
+		_.forOwn(data.properties, (value, key) => {
+			console.log('key', key)
+			schema[key] = {}
+			dealSchema(value, schema[key])
+		})
+	}
+	// 拥有id表示是model里自定义的对象
+	if (_.has(data, 'id')) {
+		schema[data.type] = {}
+		dealSchema(allModel[data.type], schema[data.type])
+	}
+
+	if (data.type === 'array' && _.has(data, 'items')) {
+		schema.type = 'array'
+		schema.items = {}
+		dealSchema(data.items, schema.items)
+	}
+
+	data.type ? schema.type = data.type : null
+
+	return schema
+}
+
+// createSchema(testData)
+console.log('result', dealSchema(
+	{
+		properties: {
+			id: {
+				type: 'string'
+			},
+			name: {
+				type: 'string'
+			}
+		}
+	}
+))
+
 
 
 

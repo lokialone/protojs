dae7948046fd3ea00403b37f93e84711d3edcd36
loki <648875862@qq.com>
2018-12-19 16:56:47 +0800

    feat: remove odin

--- algorithm/leetcode/string.js
+++ algorithm/leetcode/string.js
@@ -108,5 +108,40 @@ var firstUniqChar1 = function(s) {
 };
 
 
-console.log(firstUniqChar1("loveleetcode"))
+// console.log(firstUniqChar1("loveleetcode"))
 
+// 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的一个字母异位词。
+// 示例 1:
+// 输入: s = "anagram", t = "nagaram"
+// 输出: true
+// 示例 2:
+// 输入: s = "rat", t = "car"
+// 输出: false
+// 说明:
+// 你可以假设字符串只包含小写字母。
+// 进阶:
+// 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    for (let i = 0, len = s.length; i < len; i++) {
+        if (t.indexOf(s[i]) === -1) return false
+        t = t.replace(s[i], '')
+    }
+    return true
+};
+
+var isAnagram1 = function(s, t) {
+    var len=s.length
+    for(var i=0;i<len;i++){
+        if(s == t || s.length!=t.length) break;
+        t=t.replace(new RegExp(s[0],'g'),"");        
+        s=s.replace(new RegExp(s[0],'g'),"");
+    }
+    return s==t? true:false;
+};
+console.log(isAnagram1("anagram", "nagaram"))
---
+++ webpack-test/src/hello.js
@@ -0,0 +1,7 @@
+export function hello() {
+    console.log('hello.js');
+}
+
+// exports.hello = function () {
+//     return 1;
+// }
